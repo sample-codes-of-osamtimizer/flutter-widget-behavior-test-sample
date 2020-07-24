ea398ce9a94b880599c0a5516f1d85675f2df615
tokky <otokita@pepabo.com>
2020-07-24 15:41:36 +0900

    add initialization test of home widget

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,6 +1,22 @@
+import 'package:behavior_test/mail_list.dart';
+import 'package:behavior_test/mail_store.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:behavior_test/main.dart';
+import 'package:provider/provider.dart';
 
-void main() {}
+void main() {
+  testWidgets("fetch mails after MailList widget initialization",
+      (WidgetTester tester) async {
+    final store = MailStore();
+    expect(store.mails.length, 0);
+    await tester.pumpWidget(
+      ChangeNotifierProvider.value(
+        value: store,
+        child: Home(),
+      ),
+    );
+    expect(store.mails.length, 20);
+  });
+}
